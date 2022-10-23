e27eb538dc7e6c46bc649243e7fc0fe515271aba
Mahir Molhtar <mahr.bwb@gmail.com>
2022-10-24 02:06:07 +0300

    add some const values

--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -16,12 +16,14 @@ class AppPadding {
 }
 class AppSize {
   static const double s0 = 0;
+  static const double s0_2 = 0.2;
+  static const double s0_25 = 0.25;
   static const double s1_5 = 1.5;
   static const double s4 = 4.0;
   static const double s12 = 12.0;
   static const double s14= 14.0;
   static const double s16 = 16.0;
-  static const double p18 = 18.0;
+  static const double s18 = 18.0;
   static const double s20 = 20.0;
   static const double s60 = 60.0;
   static const double s100 = 100.0;
