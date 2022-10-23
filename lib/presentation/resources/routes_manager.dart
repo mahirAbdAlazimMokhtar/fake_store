ef504a6c903a999a71643b30d4451b00dc94cd99
Mahir Molhtar <mahr.bwb@gmail.com>
2022-10-24 02:05:52 +0300

    make routes for app

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:store_rest_api/presentation/home_screen/view/home_screen.dart';
+import 'package:store_rest_api/presentation/resources/strings_manager.dart';
 
 
 class Routes {
-  static const String splashRoute = "/";
-  static const String loginRoute = "/login";
-  static const String registerRoute = "/register";
-  static const String forgetPasswordRoute = "/forgetPassword";
-  static const String homeRoute = "/home";
-  static const String onBoardingRoute = "/onBoarding";
-  static const String storeDetailsRoute = "/storeDetails";
+  //static const String splashRoute = "/";
+  static const String homeScreen = "/";
+  // static const String registerRoute = "/register";
+  // static const String forgetPasswordRoute = "/forgetPassword";
+  // static const String homeRoute = "/home";
+  // static const String onBoardingRoute = "/onBoarding";
+  // static const String storeDetailsRoute = "/storeDetails";
 }
 
 class RouteGenerator {
-  // static Route<dynamic> getRoute(RouteSettings settings) {
-  //   switch (settings.name) {
-  //     case Routes.splashRoute:
-  //       return MaterialPageRoute(builder: (_) => const SplashView());
+  static Route<dynamic> getRoute(RouteSettings settings) {
+   switch (settings.name) {
+    case Routes.homeScreen:
+    return MaterialPageRoute(builder: (_) =>  HomeScreen());
   //     case Routes.loginRoute:
   //       return MaterialPageRoute(builder: (_) => const LoginView());
   //     case Routes.registerRoute:
@@ -28,16 +30,16 @@ class RouteGenerator {
   //       return MaterialPageRoute(builder: (_) => const StoreDetailsView());
   //       case Routes.onBoardingRoute:
   //       return MaterialPageRoute(builder: (_) => const OnBoardingView());
-  //     default:
-  //       return unDefinedRoute();
-  //   }
-  // }
-  // static Route<dynamic>unDefinedRoute (){
-  //   return MaterialPageRoute(builder: (_)=> Scaffold(
-  //     appBar: AppBar(
-  //       title: const Text(AppStrings.noRouteFound),
-  //     ),
-  //     body: const Center(child: Text(AppStrings.noRouteFound),),
-  //   ));
-  // }
+     default:
+       return unDefinedRoute();
+    }
+   }
+  static Route<dynamic>unDefinedRoute (){
+    return MaterialPageRoute(builder: (_)=> Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStrings.noRouteFound),
+      ),
+      body: const Center(child: Text(AppStrings.noRouteFound),),
+    ));
+  }
 }
