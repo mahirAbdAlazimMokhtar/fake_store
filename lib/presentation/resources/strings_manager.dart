182ba3b5b25adb7bd52f87402ec69f07b4bd7050
Mahir Molhtar <mahr.bwb@gmail.com>
2022-11-08 10:27:20 +0300

    add some string value

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,9 +1,23 @@
 class AppStrings {
   static const String homePage = 'Home';
+  static const String userName = 'User Name';
+  static const String email = 'email@email.com';
+  static const String userRole = 'User Role';
+  static const String category = 'Category';
+  static const String categories = 'Categories';
+  static const String users = 'All Users';
+  static const String loremIpsum = 'Lorem Ipsum';
+  static const String productPrice = '168.00';
+  static const String description = 'Description';
+  static const String latestProducts = 'Latest Products';
+  static const String allProducts = 'All Products';
   static const String noRouteFound = 'No Route Found';
   static const String search = 'Search';
   static const String special = 'Get The Special discount';
   static const String off = '50 %\n OFF';
+  static const String percent = '\$';
+  static const String feedsPrice = '189.0';
+  static const String feedsTitle = 'Title';
 
 
 }
