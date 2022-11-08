85efd139cae86dfbaa56661ed2bf0a092112edbb
Mahir Molhtar <mahr.bwb@gmail.com>
2022-11-08 10:25:07 +0300

    added constant value

--- lib/presentation/home_screen/constants/constant.dart
+++ lib/presentation/home_screen/constants/constant.dart
@@ -1,1 +1,8 @@
  const int itemCount = 3;
+ const double  itemWidth = 350.0;
+ const double  scale = 0.9;
+ const int  numberOfMaxLine = 1;
+ const double  mainAxisSpacing = 8;
+ const double  childAspectRatio = 0.65 ;
+ const double  crossAxisSpacing=10 ;
+ const int crossAxisCount= 2 ;
--- lib/presentation/resources/constants_manager.dart
+++ lib/presentation/resources/constants_manager.dart
@@ -1,4 +1,5 @@
 class AppConstants{
   static const int splashDelay = 2;
   static const int animationTimer = 200;
+  static const double opacity_05 = 0.5;
 }
